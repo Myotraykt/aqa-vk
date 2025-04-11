8318954024df1984fc68ae28a69f08bb7dbddf05
Артем Иванов <myotraykt@yandex.ru>
2025-04-11 18:04:54 +0300

    add to github

--- tests/widget.spec.ts
+++ tests/widget.spec.ts
@@ -14,7 +14,7 @@ test.describe('Uchi.ru widget ', () => {
     await page.click('._UCHI_COOKIE__button');
   });
 
-  test('opens', async ({page}) => {
+  test('opens', async ({ page }) => {
     await widgetPage.openWidget();
 
     await expect(widgetPage.getWidgetBody()).toBeVisible()
@@ -23,12 +23,26 @@ test.describe('Uchi.ru widget ', () => {
   test('has correct title', async ({ page }) => {
     await widgetPage.openWidget();
 
-    const articles = await widgetPage.getPopularArticles();
+    const articlesList = widgetPage.wrapper().locator(WidgetPage.selector.ARTICLE_POPULAR_LIST);
+    await expect(articlesList).toBeVisible();
 
+    const articles = await widgetPage.getPopularArticles();
+    expect(articles.length).toBeGreaterThan(0);
+
+    await articles[0].waitFor();
     await articles[0].click();
 
     await widgetPage.clickWriteToUs();
 
-    expect(await widgetPage.getTitle()).toEqual('Связь с поддержкой');
+    await expect(await widgetPage.getTitle()).toEqual('Связь с поддержкой');
+  });
+
+  // новый тест, проверяет корректность заголовков популярных статей
+  test('has proper popular articles heading', async ({ page }) => {
+    await widgetPage.openWidget();
+  
+    const popularTitle = widgetPage.wrapper().locator(WidgetPage.selector.ARTICLE_POPULAR_TITLE);
+    await expect(popularTitle).toBeVisible();
+    await expect(popularTitle).toHaveText('Популярные статьи');
   });
 });
